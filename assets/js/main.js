761a784babfaf14eea11d897eb25902e6153b0c6
Muhammad Rizky Habibi <rizkyhabibi09@gmail.com>
2022-05-29 16:50:12 +0700

    link donation

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,23 +69,23 @@
 
     /* Back Top Link active
     ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
+      // var offset = 200;
+      // var duration = 500;
+      // $(window).scroll(function() {
+      //   if ($(this).scrollTop() > offset) {
+      //     $('.back-to-top').fadeIn(400);
+      //   } else {
+      //     $('.back-to-top').fadeOut(400);
+      //   }
+      // });
 
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
+      // $('.back-to-top').on('click',function(event) {
+      //   event.preventDefault();
+      //   $('html, body').animate({
+      //     scrollTop: 0
+      //   }, 600);
+      //   return false;
+      // });
 
 
 
